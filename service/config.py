790807bcc2d92e932094c147aaff4cb8ed130f37
Falconfeather4 <huangsimiao2014@gmail.com>
2023-07-19 12:49:39 -0700

    added gui

--- service/config.py
+++ service/config.py
@@ -1,13 +1,20 @@
 # stores and initializes global variables
 import numpy as np
 
+# set the below values to True to turn on of False to turn off
 axis = True
 eigenvectors = True
 grid_lines = True
 i_j_hat = True
 determinant = True
-transformation_matrix = np.array([[1, 0], [0, 1]])
-image_path = "images/ubc_logo.jpg"
+
+# this determins the spacing between the tick marks / grid lines / size of the
+# determinant. It is measured in pixels.
 unit_length = 80
+
+transformation_matrix = np.array([[1, 0.5], [-0.5, 1]])
+image_names = ["ubc_logo.jpg", "bunny.jpg"]
+image_root_path = "images/"
+
 
 
--- service/main.py
+++ service/main.py
@@ -2,68 +2,163 @@ import numpy as np
 import image_transformation
 import cv2
 import pygame
-from ui import *
+import pygame_gui
 import config
+from pygame_gui.elements import UIDropDownMenu
+from pygame_gui.elements import UILabel
+from pygame_gui.elements import UITextEntryLine
 
-# starts the program
-start()
-
-# loads img from path stored in config
-img = cv2.imread(config.image_path)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-
-# resizing img
-rows, cols, layers = img.shape
-if (rows > 600 or cols > 600):
-    if (rows > cols):
-        height = 600;
-        width = int(height * (cols / rows))
-    else:
-        width = 600;
-        height = int(width * (cols / rows))
-    img = cv2.resize(img, (width, height))
-rows, cols, layers = img.shape
+# resizes img
+def resize_image(img, size):
+    rows, cols, layers = img.shape
+    if (rows > size or cols > size):
+        if (rows > cols):
+            height = size;
+            width = int(height * (cols / rows))
+        else:
+            width = size;
+            height = int(width * (cols / rows))
+        img = cv2.resize(img, (width, height))
+    return img
 
 
-# creating matrices used in transformation
-frame_num = 70
-matrix = config.transformation_matrix
 identity_matrix = np.array([[1, 0], [0, 1]])
-delta_matrix = matrix - identity_matrix
-step_matrix = 1 / frame_num * delta_matrix;
+frame_num = 70
+# calculates step matrix (which is used in transformation)
+def calculate_step_matrix():
+    matrix = config.transformation_matrix
+    delta_matrix = matrix - identity_matrix
+    step_matrix = 1 / frame_num * delta_matrix;
+
+    return step_matrix
 
 
-# creating frames used in animation
-frames = []
-for i in range(frame_num):
-    transf_matrix = identity_matrix + i * step_matrix
-    transformed_img = image_transformation.do_transformations(img, transf_matrix)
-    frames.append(transformed_img)
 
+
+
+img = config.image_root_path + config.image_names[0]
+rows = 0
+cols = 0
+layers = 0
 
 # initializing pygame
 pygame.init()
-window = pygame.display.set_mode((cols + cols / 50, rows + rows / 50))
+pygame.display.set_caption('Linear Transformations')
+window = pygame.display.set_mode((1100, 800))
+
 clock = pygame.time.Clock()
+
+manager = pygame_gui.UIManager((1100, 800))
+
+play_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((775, 700), (100, 50)),
+                                           text='Play',
+                                           manager=manager)
+image_dropdown = UIDropDownMenu(options_list=config.image_names, starting_option=config.image_names[0],
+                                relative_rect=pygame.Rect(50, 50, 400, 50),
+                                manager=manager, expansion_height_limit=100)
+
+
+matrix_label = UILabel(text="Transformation Matrix:", relative_rect=pygame.Rect((50, 220), (200, 50),
+                       manager=manager))
+top_left_matrix_input = UITextEntryLine(relative_rect=pygame.Rect((300, 210), (50, 40)))
+top_right_matrix_input = UITextEntryLine(relative_rect=pygame.Rect((355, 210), (50, 40)))
+bottom_left_matrix_input = UITextEntryLine(relative_rect=pygame.Rect((300, 255), (50, 40)))
+bottom_right_matrix_input = UITextEntryLine(relative_rect=pygame.Rect((355, 255), (50, 40)))
+
+
+unit_length_lable = UILabel(text="Unit Length:", relative_rect=pygame.Rect((50, 350), (100, 40)),manager=manager)
+unit_length_input = UITextEntryLine(relative_rect=pygame.Rect((170, 350), (50, 40)))
+
+
+axis_label = UILabel(text="Axis:", relative_rect=pygame.Rect((50, 460), (100, 40)), manager=manager)
+eigenvector_label = UILabel(text="Eigenvectors:", relative_rect=pygame.Rect((50, 520), (110, 40)), manager=manager)
+grid_lines_label = UILabel(text="Grid Lines:", relative_rect=pygame.Rect((50, 580), (100, 40)) ,manager=manager)
+i_j_hat_label = UILabel(text="i-j hat:", relative_rect=pygame.Rect((50, 640), (100, 40)), manager=manager)
+determinant_label = UILabel(text="Determinant:", relative_rect=pygame.Rect((50, 700), (100, 40)), manager=manager)
+
+
+yes_no_options = ["yes", "no"]
+axis_dropdown = UIDropDownMenu(options_list=yes_no_options, starting_option="yes",
+                                relative_rect=pygame.Rect(170, 460, 280, 40),
+                                manager=manager, expansion_height_limit=100)
+eigenvector_dropdown = UIDropDownMenu(options_list=yes_no_options, starting_option="yes",
+                                relative_rect=pygame.Rect(170, 520, 280, 40),
+                                manager=manager, expansion_height_limit=100)
+grid_lines_dropdown = UIDropDownMenu(options_list=yes_no_options, starting_option="yes",
+                                relative_rect=pygame.Rect(170, 580, 280, 40),
+                                manager=manager, expansion_height_limit=100)
+i_j_hat_dropdown = UIDropDownMenu(options_list=yes_no_options, starting_option="yes",
+                                relative_rect=pygame.Rect(170, 640, 280, 40),
+                                manager=manager, expansion_height_limit=100)
+determinant_dropdown = UIDropDownMenu(options_list=yes_no_options, starting_option="yes",
+                                      relative_rect=pygame.Rect(170, 700, 280, 40),
+                                      manager=manager, expansion_height_limit=100)
+
+
+
 index = 0
 running = True
+playing = False
 
-
+last_display_surface = None
 # display animation
 while running:
-    clock.tick(20)
+    window.fill(pygame.Color(128, 128, 128, 255))
+    time_delta = clock.tick(30) / 1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if index >= len(frames):
-        index = len(frames)
-        continue
-    display_img = cv2.transpose(frames[index])
-    pygame_surface = pygame.surfarray.make_surface(display_img)
-    x = 0
-    y = 0
-    window.blit(pygame_surface, (x, y))
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == play_button:
+                playing = True
+
+                img = cv2.imread(config.image_root_path + image_dropdown.selected_option)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img = resize_image(img, 600)
+
+                tl_matrix_input = float(top_left_matrix_input.get_text())
+                tr_matrix_input = float(top_right_matrix_input.get_text())
+                bl_matrix_input = float(bottom_left_matrix_input.get_text())
+                br_matrix_input = float(bottom_right_matrix_input.get_text())
+                config.transformation_matrix = np.array([[tl_matrix_input, tr_matrix_input],
+                                                        [bl_matrix_input, br_matrix_input]])
+
+                config.unit_length = int(unit_length_input.get_text())
+
+                config.axis = True if axis_dropdown.selected_option == "yes" else False
+                config.eigenvectors = True if eigenvector_dropdown.selected_option == "yes" else False
+                config.grid_lines = True if grid_lines_dropdown.selected_option == "yes" else False
+                config.i_j_hat = True if grid_lines_dropdown.selected_option == "yes" else False
+                config.determinant = True if determinant_dropdown.selected_option == "yes" else False
+
+
+        manager.process_events(event)
+
+    if playing:
+        if index >= frame_num:
+            index = 0
+            playing = False
+            continue
+
+        step_matrix = calculate_step_matrix()
+        transf_matrix = identity_matrix + index * step_matrix
+        transformed_img = image_transformation.do_transformations(img, transf_matrix)
+
+        display_img = cv2.transpose(transformed_img)
+        pygame_surface = pygame.surfarray.make_surface(display_img)
+        last_display_surface = pygame_surface
+        x = 600
+        y = 50
+        window.blit(pygame_surface, (x, y))
+
+        index += 1
+    elif not (last_display_surface is None):
+        x = 600
+        y = 50
+        window.blit(pygame_surface, (x, y))
+
+    manager.update(time_delta)
+    manager.draw_ui(window)
     pygame.display.update()
 
-    index += 1
+pygame.quit()
